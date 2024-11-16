aec15ee9fbf399206c5840359a55dab951c7a627
marco oldani <marcoooldani@gmail.com>
2024-11-16 12:47:10 +0100

    tailwind init

---
+++ postcss.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    plugins: {
+      tailwindcss: {},   // Attiva Tailwind CSS
+      autoprefixer: {},  // Attiva Autoprefixer per la compatibilità con i browser
+    },
+  }
+  
