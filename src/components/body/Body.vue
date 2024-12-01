8f58fe9fe55783565555d2a3b6b4a4f1aeb3ea9f
marco oldani <marcoooldani@gmail.com>
2024-12-01 23:39:31 +0100

    fixed project card component rendering bug (z  value issue) and made project cards animation in the body

--- src/components/body/Body.vue
+++ src/components/body/Body.vue
@@ -1,10 +1,10 @@
 <template>
-<div class="bg-background-Lmode relative -z-20">
+<div class="bg-background-Lmode relative">
     <div class="  w-full h-auto p-10">
         <Scene3D></Scene3D>
     </div>
 
-    <div class="flex flex-row justify-between w-full h-full absolute bg-background-Lmode -z-10">
+    <div class="flex flex-row justify-between w-full h-full absolute bg-background-Lmode">
         <div id="sideSlidingPanel_left"
             class="border-r-8 border-r-accent-Lmode w-1/5">
         </div>
@@ -13,12 +13,13 @@
         </div>
     </div>
 
-    <div>
-        <ProjectCardVue></ProjectCardVue>
-        <ProjectCardVue></ProjectCardVue>
-        <ProjectCardVue></ProjectCardVue>
-    </div>
-
+    <Transition name="cardsAnimation">
+        <div v-if="showProjectSection" class=" relative">
+                <ProjectCardVue class=" hover:rotate-3"></ProjectCardVue>
+                <ProjectCardVue class=""></ProjectCardVue>
+                <ProjectCardVue class=""></ProjectCardVue>
+        </div>
+    </Transition>
         
     
 </div>
@@ -27,13 +28,53 @@
 <script>
 import Scene3D from "@/components/body/Scene3D.vue"
 import ProjectCardVue from './ProjectCard.vue'
+import { ref } from 'vue'
 export default {
     components: {
         Scene3D, ProjectCardVue
     },
+    setup(){
+        let showProjectSection = ref(false)
+
+        function handle_showCurrentHomeSection(e){
+            if(e.clientX < window.innerWidth/2){
+                showProjectSection.value = true
+            }
+            else{
+                showProjectSection.value = false
+            }
+        }
+
+        document.onmousemove = handle_showCurrentHomeSection
+
+        return {showProjectSection}
+    }
 }
 </script>
 
 <style>
+.cardsAnimation-enter-from {
+    translate: -100px;
+    opacity: 0%;
 
+}
+.cardsAnimation-enter-to {
+    translate: 0px;
+    opacity: 100%;
+
+}
+.cardsAnimation-enter-active
+ {
+  transition: all 1s;
+  transition-timing-function: cubic-bezier(0.05, 0.95, 0.05, 1);
+}
+
+.cardsAnimation-leave-to {
+    opacity: 0%;
+
+}
+.cardsAnimation-leave-active{
+    transition: all 0.5s;
+}
+    
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="header">
-    <Header/>
+  <div>
+    <Header></Header>
     <Body/>
   </div>
 </template>
