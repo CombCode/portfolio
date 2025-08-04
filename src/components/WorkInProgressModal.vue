e1a8a1f3471fecc034b6bf42e7de5e3b34a49587
CombCode <marcoooldani@gmail.com>
2025-08-04 13:43:11 +0200

    feat(UI/FrontEnd-contactForm): send button icon animation, some style changes and form attributes to store messages on Netlify

--- src/components/WorkInProgressModal.vue
+++ src/components/WorkInProgressModal.vue
@@ -5,9 +5,9 @@
                 <div class=""><h1 class=" text-5xl text-accent-Lmode relative z-50"> WORK IN PROGRESS</h1></div>                       
                 <p class=" flex-grow py-10 text-xl text-left z-10"> Please keep in mind that this is a personal project and is subject to changes as I change.<br><br>
                     Since it's a portfolio, it's meant to grow and evolve as I gain more experience, learn and build new projects. </p>
+                <p class=" flex-grow py-10 text-xl text-left z-10"> This is still a work in progress. Some parts might be incomplete, but I’m on it! </p>
                 <hr class=" border-accent-Lmode dark:border-white z-10">
                 <p class=" bg-gray-100 z-40 -mx-10 -mb-10 pb-10 pt-4"> Thanks for your understanding! </p>
-                
             </div>
             <img src="@/assets/gear.svg" class="absolute h-full w-full left-1/2 opacity-100 overflow-hidden transition-all ease-in-out duration-1000 z-0" :style="{ transform: `translateX(-100%) translateY(-60%) scale(100%) rotate(${gearAngle}deg)` }"/>
             <img src="@/assets/gear.svg" class="absolute h-full w-full left-1/2 opacity-100 overflow-hidden transition-all ease-in-out duration-1000 z-0" :style="{ transform: `translateX(-20%) translateY(-100%) scale(150%) rotate(${gearAngle}deg)` }"/>
--- src/components/contacForm.vue
+++ src/components/contacForm.vue
@@ -1,19 +1,29 @@
 <template>
     <div class=" w-auto h-auto bg-primary-Lmode rounded-xl shadow">
-        <div class="flex flex-col p-10 ml-40">
-            <div v-for="(field, index) in fields" :key="index">
-                <div class=" py-5">
-                    <label :for="field.name" class=" block text-sm text-text-Lmode text-left w-full">{{ field.name }}</label>
-                    <textarea  v-if="field.type === 'textarea'"
-                    class=" w-full h-32 rounded-md shadow p-2 align-top text-left resize-none"
-                    ></textarea>
-                    <input v-else :type="field.type" :name="field.name" :placeholder="field.placeholder" class=" rounded-md shadow p-2">                    
+        <div class="flex flex-col p-10">
+            <form name="messagesForm" method="POST" data-netlify="true">
+                <input type="hidden" name="form-name" value="messagesForm">
+                <div v-for="(field, index) in fields" :key="index">
+                    <div class=" py-5">
+                        <label :for="field.name" class=" block text-sm text-text-Lmode text-left w-full">{{ field.name }}</label>
+                        <textarea  v-if="field.type === 'textarea'"
+                        class=" w-full h-32 rounded-md shadow p-2 align-top text-left resize-none"
+                        ></textarea>
+                        <input v-else :type="field.type" :name="field.name" :placeholder="field.placeholder" class=" rounded-md shadow p-2 w-64">                    
+                    </div>
                 </div>
-            </div>
-            <div class=" flex flex-row align-middle justify-center mt-10 w-auto rounded sendShadows border-b-2 border-black hover:scale-110 active:scale-100 transition-all ease-in-out cursor-pointer">
-                <p class=" font-bold text-black">Send</p>
-                <Send color="black" stroke-width="2"/>  
-            </div>
+                <div class="group overflow-hidden p-2 flex flex-row align-middle justify-center mt-10 w-auto rounded sendShadows border-b-2 border-black hover:scale-110 active:scale-100 transition-all ease-in-out cursor-pointer"
+                @click="submitAnimation"
+                type="submit">
+                    <p class=" font-bold text-black">Send</p>
+                    <Send color="black" stroke-width="2" class=" transition-all ease-in-out duration-500 translate-y-20 -translate-x-20"
+                    :class="{
+                        'group-hover:translate-y-0 group-hover:translate-x-0' : !sendIconanimationIsActive&&!resetIconPosition,
+                        '-translate-y-20 translate-x-20':sendIconanimationIsActive,
+                        'translate-y-20 -translate-x-20 transition-none': resetIconPosition
+                    }"/>  
+                </div>
+            </form>
         </div>
     </div>
 </template>
@@ -34,7 +44,22 @@ export default {
             { name: 'Message', type: 'textarea', placeholder: 'Your Message' }
         ]);
 
-        return { fields };
+        let sendIconanimationIsActive = ref(false);
+        let resetIconPosition = ref(false);
+        const submitAnimation = () => {
+            sendIconanimationIsActive.value = true;
+            setTimeout(() => {
+                resetIconPosition.value = true;
+                setTimeout(() => {
+                    resetIconPosition.value = false;
+                }, 500); // Reset position after 0.5 seconds
+                sendIconanimationIsActive.value = false;
+            }, 1000); // Reset after 1 second
+            console.log('Form submitted');
+            context.emit('close'); // Emit an event to close the form
+        };
+
+        return { fields, submitAnimation, sendIconanimationIsActive, resetIconPosition};
     }
 }
 </script>
